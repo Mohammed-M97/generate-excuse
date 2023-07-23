7484ac8bdc298e38130c11a66ffa96bbb7f94bef
Mohammed Al-Awlaqi <mohammed-alawlaqi@outlook.com>
2023-07-23 20:40:24 +0300

    add everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+
+  const [exuces, setExuces] = useState('')
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}`)
+    .then((res) => setExuces(res.data[0].excuse))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Generate An Excuse</h1>
+
+      <button onClick={() => fetchExcuse("party")}> Party </button>
+      <button onClick={() => fetchExcuse("family")}> Family </button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+
+      <h1> {exuces} </h1>
     </div>
-  );
+    )
 }
+
+
 
 export default App;
